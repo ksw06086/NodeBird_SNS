240a9fe037af444866a127cfbacb4a6e469ffe8c
ksw06086 <k-sunwo@naver.com>
2023-07-30 11:25:55 +0900

    nodebird - 팔로우취소 기능 추가

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const { isLoggedIn } = require('../middlewares');
-const { follow } = require('../controllers/user');
+const { follow, followCancel } = require('../controllers/user');
 const router = express.Router();
 
 router.post('/:id/follow', isLoggedIn, follow);
+router.post('/:id/follow/cancel', isLoggedIn, followCancel);
 
 module.exports = router;
